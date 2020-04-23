ce3c99f3914d0f8e65ecee2ce490856885bd3ba8
Hsing Yu <hsingyuc.7@gmail.com>
2020-04-23 19:16:10 +0300

    Create workout timer and pause time

--- frontend/src/Workout.js
+++ frontend/src/Workout.js
@@ -4,29 +4,123 @@ import Countdown from './Countdown';
 class Workout extends React.Component {
 	constructor( props ) {
 		super( props );
-		this.state = {
-			time: 10,
+		this.workout = {
+			key: 'repeaters',
+			name: 'Repeaters',
+			readyTime: 5,
+			sets: 3,
+			setsRestTime: 5,
+			repTime: 2,
+			repsRestTime: 3,
+			reps: 6,
 		};
-
-		this.start = this.start.bind( this );
+		this.state = {
+			repCount: 0,
+			setCount: 0,
+			current: 'Ready',
+			time: this.workout.readyTime,
+		};
+		this.timeout = null;
+		this.startTimer = this.startTimer.bind( this );
+		this.pauseTimer = this.pauseTimer.bind( this );
 	}
 
-	start() {
-		const { time } = this.state;
-		if ( time === 0 ) {
+	startTimer() {
+		const {
+			time,
+			current: previousStep,
+			repCount,
+			setCount,
+		} = this.state;
+
+		// Workout is finished.
+		if ( setCount === 3 ) {
+			this.setState( {
+				setCount: 0,
+			} );
 			return;
 		}
+
+		// Completed a set.
+		if ( repCount === 6 ) {
+			this.setState( {
+				time: this.workout.setsRestTime,
+				current: 'rest',
+				setCount: setCount + 1,
+				repCount: 0,
+			}, () => {
+				this.timeout = setTimeout( () => this.startTimer(), 1000 );
+			} );
+			return;
+		}
+
+		if ( time === 0 ) {
+			// Start workout
+			if ( previousStep === 'ready' ) {
+				this.setState( {
+					time: this.workout.repTime,
+					current: 'rep',
+				}, () => {
+					this.timeout = setTimeout( () => this.startTimer(), 1000 );
+				} );
+				return;
+			}
+
+			// Start a rest period.
+			if ( previousStep === 'rep' ) {
+				this.setState( {
+					time: this.workout.repsRestTime,
+					current: 'rest',
+				}, () => {
+					this.timeout = setTimeout( () => this.startTimer(), 1000 );
+				} );
+				return;
+			}
+
+			// Start a rep.
+			if ( previousStep === 'rest' ) {
+				this.setState( {
+					time: this.workout.repTime,
+					current: 'rep',
+					repCount: repCount + 1,
+				}, () => {
+					this.timeout = setTimeout( () => this.startTimer(), 1000 );
+				} );
+				return;
+			}
+		}
 		this.setState( { time: time - 1 } );
-		setTimeout( () => this.start(), 1000 );
+		this.timeout = setTimeout( () => this.startTimer(), 1000 );
+	}
+
+	pauseTimer() {
+		clearTimeout( this.timeout );
 	}
 
 	render() {
-		const { time } = this.state;
+		const {
+			time,
+			repCount,
+			setCount,
+			current,
+		} = this.state;
 		return (
 			<div>
+				<span>
+					Status:
+					{current}
+				</span>
 				<Countdown time={time} />
-
-				<button type="button" onClick={this.start}>Start</button>
+				<span>
+					Reps:
+					{ Math.min( repCount + 1, 6 ) } / 6
+				</span>
+				<span>
+					Sets:
+					{ Math.min( setCount + 1, 3 ) } / 3
+				</span>
+				<button type="button" onClick={this.startTimer}>Start</button>
+				<button type="button" onClick={this.pauseTimer}>Pause</button>
 			</div>
 		);
 	}
