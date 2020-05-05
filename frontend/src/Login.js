cb20aea9d14827c2ccb8c79f06f596f081aa3343
Hsing Yu <hsingyuc.7@gmail.com>
2020-05-05 15:52:10 +0300

    Import formik and replace state

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -7,45 +7,40 @@ import Container from '@material-ui/core/Container';
 import { Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import { Formik } from 'formik';
 import actions from './actions';
 
 class Login extends React.Component {
 	constructor( props ) {
 		super( props );
-		this.state = {
-			email: '',
-			password: '',
-			error: '',
-		};
-		this.handleSubmit = this.handleSubmit.bind( this );
+		this.onSubmit = this.onSubmit.bind( this );
 	}
 
-	async handleSubmit( event ) {
-		event.preventDefault();
-		const { email, password } = this.state;
-		const data = {
-			email,
-			password,
-		};
+	async onSubmit( values, { setErrors } ) {
+		// values.preventDefault();
+		const { email, password } = values;
+
 		const response = await fetch( 'http://localhost:8080/auth/login', {
 			method: 'POST',
 			headers: {
 				'Content-Type': 'application/json',
 			},
 			credentials: 'include',
-			body: JSON.stringify( data ),
+			body: JSON.stringify( {
+				email,
+				password,
+			} ),
 		} );
 		const json = await response.json();
 		const { setCurrentUser } = this.props;
 		if ( json.error ) {
-			this.setState( { error: json.error } );
+			setErrors( json.error.errors );
 		} else if ( json.data ) {
 			setCurrentUser( json.data.user );
 		}
 	}
 
 	render() {
-		const { error } = this.state;
 		const { currentUser } = this.props;
 
 		if ( currentUser ) {
@@ -56,51 +51,71 @@ class Login extends React.Component {
 			<Container component="main" maxWidth="xs">
 				<CssBaseline />
 				<div>
-					<form noValidate>
-						<TextField
-							variant="outlined"
-							margin="normal"
-							required
-							fullWidth
-							id="email"
-							label="Email Address"
-							name="email"
-							autoComplete="email"
-							autoFocus
-							onChange={ (event) => this.setState({ email: event.target.value }) }
-							helperText={ error }
-						/>
-						<TextField
-							variant="outlined"
-							margin="normal"
-							required
-							fullWidth
-							name="password"
-							label="Password"
-							type="password"
-							id="password"
-							autoComplete="current-password"
-							onChange={ (event) => this.setState({ password: event.target.value }) }
-							helperText={ error }
-						/>
-						<Button
-							onClick={ this.handleSubmit }
-							type="submit"
-							fullWidth
-							variant="contained"
-							color="primary"
-						>
-							Sign In
-						</Button>
-						{/* <Grid container>
-							<Grid item> */}
-								{/* <Link href="#" variant="body2"> */}
-						<Link href="/register">
-							Don't have an account? Sign Up
-						</Link>
-							{/* </Grid>
-						</Grid> */}
-					</form>
+					<Formik
+						initialValues={ { email: '', password: '' } }
+						validate={Login.validate}
+						onSubmit={this.onSubmit}
+					>
+						{( {
+							values,
+							errors,
+							touched,
+							handleChange,
+							handleBlur,
+							handleSubmit,
+						} ) => (
+							<form noValidate>
+								<TextField
+									error={ errors.email && touched.email }
+									variant="outlined"
+									margin="normal"
+									required
+									fullWidth
+									id="email"
+									label="Email Address"
+									name="email"
+									autoComplete="email"
+									autoFocus
+									// onChange={ (event) => this.setState({ email: event.target.value }) }
+									// helperText={ error }
+									value={ values.email }
+									onChange={handleChange}
+									onBlur={handleBlur}
+									helperText={ touched.email && errors.email }
+								/>
+								<TextField
+									error={ touched.password && errors.password }
+									variant="outlined"
+									margin="normal"
+									required
+									fullWidth
+									name="password"
+									label="Password"
+									type="password"
+									id="password"
+									autoComplete="current-password"
+									// onChange={ (event) => this.setState({ password: event.target.value }) }
+									// helperText={ error }
+									value={ values.password }
+									onChange={handleChange}
+									onBlur={handleBlur}
+									helperText={ touched.password && errors.password }
+								/>
+								<Button
+									onClick={ handleSubmit }
+									type="submit"
+									fullWidth
+									variant="contained"
+									color="primary"
+								>
+									Sign In
+								</Button>
+								<Link href="/register">
+									Don't have an account? Sign Up
+								</Link>
+							</form>
+						)}
+					</Formik>
 				</div>
 			</Container>
 		);
@@ -109,7 +124,7 @@ class Login extends React.Component {
 
 Login.propTypes = {
 	setCurrentUser: PropTypes.func.isRequired,
-	currentUser: PropTypes.objectOf( PropTypes.object ).isRequired,
+	currentUser: PropTypes.instanceOf( Object ),
 };
 
 const mapStateToProps = ( state ) => ( {
