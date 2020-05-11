6d5a98782e942202cf04fbbcb4ff7d70cf2610a3
Hsing Yu <hsingyuc.7@gmail.com>
2020-05-11 19:15:31 +0300

    Add tooltip for workout info

--- frontend/src/WorkoutCard.js
+++ frontend/src/WorkoutCard.js
@@ -10,6 +10,7 @@ import './WorkoutCard.scss';
 import HourglassFullIcon from '@material-ui/icons/HourglassFull';
 import LoopIcon from '@material-ui/icons/Loop';
 import SettingsBackupRestoreIcon from '@material-ui/icons/SettingsBackupRestore';
+import Tooltip from '@material-ui/core/Tooltip';
 
 export default class WorkoutCard extends React.Component {
 	renderInfo() {
@@ -19,14 +20,28 @@ export default class WorkoutCard extends React.Component {
 
 		return (
 			<>
-				<HourglassFullIcon />
-				{ repsRestTime
-					? `${repTime / 1000} / ${repsRestTime / 1000}`
-					: `${repTime / 1000}s`}
-				<LoopIcon />
-				{ `${reps} reps` }
-				<SettingsBackupRestoreIcon />
-				{ `${sets} sets` }
+				<Tooltip title="Time" arrow>
+					<div className="workout-info">
+						<HourglassFullIcon />
+						{ repsRestTime
+							? `${repTime / 1000} / ${repsRestTime / 1000}`
+							: `${repTime / 1000}s`}
+					</div>
+				</Tooltip>
+
+				<Tooltip title="Reps" arrow>
+					<div className="workout-info">
+						<LoopIcon />
+						{ `${reps}` }
+					</div>
+				</Tooltip>
+
+				<Tooltip title="Sets" arrow>
+					<div className="workout-info">
+						<SettingsBackupRestoreIcon />
+						{ `${sets}` }
+					</div>
+				</Tooltip>
 			</>
 		);
 	}
@@ -48,7 +63,7 @@ export default class WorkoutCard extends React.Component {
 						<Typography gutterBottom variant="h5" component="h2">
 							{ name }
 						</Typography>
-						<Typography variant="body2" color="textSecondary" component="p">
+						<Typography variant="body2" color="textSecondary" component="div">
 							{ this.renderInfo() }
 						</Typography>
 					</CardContent>
