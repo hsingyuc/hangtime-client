e52247e89f27c4d9bd7e7b3c57c215a24af717e3
Hsing Yu <hsingyuc.7@gmail.com>
2020-05-14 00:25:20 +0300

    Style status text

--- frontend/src/ProgressCircle.js
+++ frontend/src/ProgressCircle.js
@@ -2,13 +2,37 @@ import React from 'react';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import PropTypes from 'prop-types';
 
-export default class ProgressCircle extends React.PureComponent {
+export default class ProgressCircle extends React.Component {
+	renderText() {
+		const {
+			reps,
+			status,
+			currentRep,
+		} = this.props;
+
+		switch ( true ) {
+		case status === 'ready':
+			return 'Ready';
+		case status === 'rep':
+			return 'Hang time';
+		case status === 'rest' && currentRep === reps:
+			return 'Rest';
+		case status === 'rest':
+			return 'Rest';
+		default:
+			return null;
+		}
+	}
+
 	render() {
 		const { totalTime, remainingTime, status } = this.props;
 		const percentComplete = ( ( totalTime - remainingTime ) / totalTime ) * 100;
 
 		return (
 			<div className={`progress-circle is-${status}`}>
+				<span className="status-text">
+					{this.renderText()}
+				</span>
 				<CircularProgress variant="static" value={percentComplete} size={200} />
 			</div>
 		);
@@ -19,4 +43,6 @@ ProgressCircle.propTypes = {
 	totalTime: PropTypes.number.isRequired,
 	remainingTime: PropTypes.number.isRequired,
 	status: PropTypes.string.isRequired,
+	reps: PropTypes.number.isRequired,
+	currentRep: PropTypes.number.isRequired,
 };
