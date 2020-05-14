0465338887e29e1adca3ed2c12abd57cfe7d31e0
Hsing Yu <hsingyuc.7@gmail.com>
2020-05-14 20:13:12 +0300

    Style timer

--- frontend/src/Countdown.js
+++ frontend/src/Countdown.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-class Countdown extends React.PureComponent {
-	render() {
+class Countdown extends React.Component {
+	renderTime() {
 		const { time } = this.props;
+		const m = `0${Math.floor( ( time / 1000 / 60 ) )}`.toString().slice( -2 );
+		const s = `0${Math.floor( ( time / 1000 ) % 60 )}`.toString().slice( -2 );
+		const mi = `0${time}`.toString().slice( -3, -1 );
+
+		return `${m}:${s}:${mi}`;
+	}
+
+	render() {
 		return (
-			<div>
-				{ time }
+			<div className="timer">
+				{this.renderTime()}
 			</div>
 		);
 	}
