e187bece0ca09a9da4ec5a2f368a523f0ecc2389
Hsing Yu <hsingyuc.7@gmail.com>
2020-05-15 15:16:00 +0300

    Show save session buttons on completion

--- frontend/src/Workout.js
+++ frontend/src/Workout.js
@@ -202,20 +202,26 @@ class Workout extends React.Component {
 		return (
 			<div>
 				<div className="countdown-container">
-					<ProgressCircle
-						totalTime={this.getTotalTime()}
-						remainingTime={time}
-						status={status}
-						status={status}
-						currentRep={currentRep}
-					/>
+					{ status === 'complete'
+						? (
+							<div className="save-session-buttons">
+								<CheckCircleOutlineIcon onClick={() => this.saveSession( true )} />
+								<HighlightOffIcon onClick={() => this.saveSession( false )} />
+							</div>
+						)
+						: (
+							<>
+								<ProgressCircle
+									totalTime={this.getTotalTime()}
+									remainingTime={time}
+									status={status}
+									status={status}
+									currentRep={currentRep}
+								/>
+								<Countdown time={time} />
+							</>
+						)}
 
-					<Countdown time={time} />
-
-					<div className="save-session-buttons">
-						<CheckCircleOutlineIcon onClick={() => this.saveSession( true )} />
-						<HighlightOffIcon onClick={() => this.saveSession( false )} />
-					</div>
 				</div>
 
 				<div className="sets-reps-text">
